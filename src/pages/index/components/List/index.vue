34a4ddeebf693719963f37803d1cf646675b14a1
pear794 <624332112@qq.com>
2021-04-27 00:05:12 +0800

    初始化项目

---
+++ src/pages/index/components/List/index.vue
@@ -0,0 +1,45 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-04-26 23:26:10
+ * @LastEditTime: 2021-04-27 00:04:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \taro3-vue-ts\src\pages\index\components\List\index.vue
+-->
+<template>
+  <view class="container">
+    <view v-for="item in list" :key="item.id" @tap="handleClickItem(item.title)">{{item.title}}</view>
+  </view>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, onMounted, toRefs } from 'vue'
+
+export interface ListProps {
+  id: number;
+  title: string;
+}
+
+export default defineComponent({
+  name: 'List',
+  props: {
+    list: Array as PropType<ListProps[]>,
+  },
+  setup(props, { emit }) {
+    const { list } = toRefs(props)
+    onMounted(() => {
+      console.log(list)
+    })
+    const handleClickItem = (item: string) => {
+      emit('handleClickItem', item)
+    }
+    return {
+      handleClickItem,
+    }
+  }
+})
+</script>
+
+<style>
+  @import "index.module.less"
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-04-26 23:20:29
+ * @LastEditTime: 2021-04-26 23:59:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \taro3-vue-ts\src\pages\index\index.vue
+-->
+<template>
+  <view class="container">
+    <list :list="list" v-on:handleClickItem="handleClickItem"></list>
+  </view>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import List, { ListProps } from './components/List/index.vue'
+
+export default defineComponent({
+  name: 'Index',
+  components: {
+    List
+  },
+  setup() {
+    const list = ref<ListProps[]>([{
+      id: 1,
+      title: '你好'
+    }, {
+      id: 2,
+      title: '你好2'
+    }, {
+      id: 3,
+      title: '你好3'
+    }])
+    const handleClickItem = (item: string) => {
+      console.log(item)
+    }
+    return {
+      list,
+      handleClickItem
+    }
+  }
+})
+</script>
+
+<style lang="less" global>
+  .container {
+    width: 300px;
+    height: 300px;
+    background: red;
+  }
+</style>
